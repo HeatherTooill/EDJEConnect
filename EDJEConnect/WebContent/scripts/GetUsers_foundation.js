512529badcdf716845d93a6477c1a201c4943fce
HeatherTooill <heather.tooill@leadingedje.com>
2014-06-12 10:27:06 -0400

    initial commit

--- EDJEConnect/WebContent/scripts/GetUsers_foundation.js
+++ EDJEConnect/WebContent/scripts/GetUsers_foundation.js
@@ -2,11 +2,18 @@
  * 
  */
 
+
 function buildTable(){
+	var values = [];
+	
+	$.getJSON( "http://localhost:8080/user/employeedetails/2",  {format: "json"}).done(function(data){values = data;});
+	
 	var output = "";
+	
+	output += "<tr><th>" + values + "</th></tr>";
 	output += "<tr><th>Edjer</th><th>Phone Number</th></tr>";
 	
-	$.each(mylist, function(i){
+	$.each(values, function(i){
 		
 		if(i%2 == 0){
 			//even
@@ -15,8 +22,8 @@ function buildTable(){
 			output += "<tr class=\"odd\">";	
 		}
 		
-		output += "<td><a href=\"details.html\">" + mylist[i].firstName + " " + mylist[i].lastName + "</a></td>";
-		output += "<td>" + mylist[i].phoneNumber + "</td>";
+		output += "<td><a href=\"details.html\">" + values[i].firstName + " " + values[i].lastName + "</a></td>";
+		output += "<td>" + values[i].phoneNumber + "</td>";
 		output += "</tr>";
 	});
 	
